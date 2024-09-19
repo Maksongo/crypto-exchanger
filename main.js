f79754e8238d391cba0abc2d538a211858ded70e
Makson <nooplace55@gmail.com>
2024-09-20 00:16:07 +0600

    SignIn открывается и закрывается при нажатии на соответственные кнопки

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+// const SigninButton = document.querySelector("#NavbarSigninID");
+// SigninButton.addEventListener('click', () => {
+//     // popup.style.display = 'block';
+//     console.log("hahaha")
+// });
+
+
 const SigninButton = document.querySelector("#NavbarSigninID");
+const SignInCloseButton = document.querySelector(".SignIn_wrapper_form_close")
+const SignInWrapper = document.querySelector(".SignIn_wrapper");
 
 SigninButton.addEventListener('click', () => {
-    // popup.style.display = 'block';
-    console.log("hahaha")
+    SignInWrapper.style.display = "block";
+});
+
+
+SignInCloseButton.addEventListener('click', () => {
+    SignInWrapper.style.display = "none";
 });
